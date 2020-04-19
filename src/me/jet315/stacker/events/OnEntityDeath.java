df1ccca3ac82cadb0977e9f04bcf5b5a3629fcb4
Pdani001 <pecsidaniel10@gmail.com>
2020-04-19 20:10:00 +0200

    Fixed custom items dropping more than once

--- src/me/jet315/stacker/events/OnEntityDeath.java
+++ src/me/jet315/stacker/events/OnEntityDeath.java
@@ -1,6 +1,7 @@
 package me.jet315.stacker.events;
 
 import me.jet315.stacker.MobStacker;
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -46,13 +47,58 @@ public class OnEntityDeath implements Listener{
         int amountToMultiply = MobStacker.getInstance().getStackEntity().parseAmount(dead.getCustomName());
         if(amountToMultiply <=1) return 1;
         for(ItemStack i : drops){
+            if(!isMobDrop(i)) continue;
             ItemStack item = new ItemStack(i);
             item.setAmount(amountToMultiply);
             dead.getWorld().dropItem(dead.getLocation(),item);
-
         }
         return amountToMultiply;
 
+    }
+
+    private boolean isMobDrop(ItemStack i){
+        Material m = i.getType();
+        return m == Material.ROTTEN_FLESH ||
+                m == Material.BONE ||
+                m == Material.STRING ||
+                m == Material.ARROW ||
+                m == Material.GUNPOWDER ||
+                m == Material.LEATHER ||
+                m == Material.BEEF ||
+                m == Material.COOKED_BEEF ||
+                m == Material.PORKCHOP ||
+                m == Material.COOKED_PORKCHOP ||
+                m == Material.FEATHER ||
+                m == Material.CHICKEN ||
+                m == Material.COOKED_CHICKEN ||
+                m == Material.COD ||
+                m == Material.COOKED_COD ||
+                m == Material.BAMBOO ||
+                m == Material.GOLD_NUGGET ||
+                m == Material.COAL ||
+                m == Material.GLASS_BOTTLE ||
+                m == Material.GLOWSTONE_DUST ||
+                m == Material.SUGAR ||
+                m == Material.STICK ||
+                m == Material.REDSTONE ||
+                m == Material.SPIDER_EYE ||
+                m == Material.EMERALD ||
+                isMusicDisc(m);
+    }
+
+    private boolean isMusicDisc(Material m){
+        return m == Material.MUSIC_DISC_11 ||
+                m == Material.MUSIC_DISC_13 ||
+                m == Material.MUSIC_DISC_BLOCKS ||
+                m == Material.MUSIC_DISC_CAT ||
+                m == Material.MUSIC_DISC_CHIRP ||
+                m == Material.MUSIC_DISC_FAR ||
+                m == Material.MUSIC_DISC_MALL ||
+                m == Material.MUSIC_DISC_MELLOHI ||
+                m == Material.MUSIC_DISC_STAL ||
+                m == Material.MUSIC_DISC_STRAD ||
+                m == Material.MUSIC_DISC_WAIT ||
+                m == Material.MUSIC_DISC_WARD;
     }
 
 }
